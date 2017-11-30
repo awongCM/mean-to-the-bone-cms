b2f9589f0936e8090619dc1378948f35d4064133
awongCM <awong.cm@gmail.com>
2017-11-30 20:26:40 +1100

    init commits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CmsService } from './cms.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  users: Array<any>;
+
+  constructor(private _cmsService: CmsService){
+    this._cmsService.getUsers()
+      .subscribe(res => this.users = res);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
-
+import { HttpModule } from '@angular/http/src/http_module';
+import { CmsService } from './cms.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [CmsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
